11afb05471ffec4b3f73cef8d8e4eab42ec3873d
Loterio <natan.loterio@farfetch.com>
2023-10-05 11:20:42 +0100

    refactoring MainFragmentTest

--- app/src/androidTest/java/me/loterio/mysimplepomodoro/MainFragmentTest.kt
+++ app/src/androidTest/java/me/loterio/mysimplepomodoro/MainFragmentTest.kt
@@ -12,20 +12,22 @@ import androidx.test.uiautomator.Until
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainFragmentTest {
 
+    private lateinit var device: UiDevice
     private val LAUNCH_TIMEOUT: Long = 3000
     private val SAMPLE_PACKAGE = "me.loterio.mysimplepomodoro"
 
-    @Test
-    fun spinner_shows_25minutes() {
+    @Before
+    fun  setup() {
         // Arrange
         // Initialize UiDevice instance
-        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
         // Start from the home screen
         device.pressHome()
@@ -45,28 +47,29 @@ class MainFragmentTest {
 
         context.startActivity(intent)
         // Wait for the app to appear
-        device.wait(
-            Until.hasObject(By.pkg(SAMPLE_PACKAGE).depth(0)),
-            LAUNCH_TIMEOUT
-        )
+//        device.wait(Until.hasObject(By.pkg(SAMPLE_PACKAGE).depth(0)), LAUNCH_TIMEOUT)
+        device.wait(Until.hasObject(By.clazz(MainActivity::class.java).depth(0)), LAUNCH_TIMEOUT)
 
-        device.wait(
-            Until.hasObject(By.clazz(MainActivity::class.java).depth(0)),
-            LAUNCH_TIMEOUT
-        )
+    }
+
+    @Test
+    fun spinner_shows_25minutes() {
+        // Act
+        val selectedTimeTV: UiObject2? = device.wait(Until.findObject(By.textContains("Time Remaining")), LAUNCH_TIMEOUT)
+        assertThat(selectedTimeTV, notNullValue())
 
         // Act
-        val spinner25: UiObject2? = device.findObject(By.text("25"))
-        assertThat(spinner25, notNullValue())
+        val spinner: UiObject2? = device.wait(Until.findObject(By.text("Select Time")), LAUNCH_TIMEOUT)
+        assertThat(spinner, notNullValue())
 
-        spinner25?.click()
+        spinner?.click()
 
-        device.wait(
-            Until.hasObject(By.text("35")),
-            LAUNCH_TIMEOUT
-        )
+        val spinnerOption: UiObject2? = device.wait(Until.findObject(By.text("25")), LAUNCH_TIMEOUT)
+        assertThat(spinnerOption, notNullValue())
 
-        val spinner35: UiObject2? = device.findObject(By.text("35"))
+        spinnerOption?.click()
+
+        val spinner35 = device.wait(Until.findObject(By.text("25")), LAUNCH_TIMEOUT)
         assertThat(spinner35, notNullValue())
     }
 
@@ -74,68 +77,21 @@ class MainFragmentTest {
     @Test
     fun spinner_shows_55minutes() {
         // Arrange
-        // Initialize UiDevice instance
-        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-
-        // Start from the home screen
-        device.pressHome()
-
-        // Wait for launcher
-        val launcherPackage: String = device.launcherPackageName
-        assertThat(launcherPackage, notNullValue())
-        device.wait(
-            Until.hasObject(By.pkg(launcherPackage).depth(0)),
-            LAUNCH_TIMEOUT
-        )
-
-        // Launch the app
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        val intent = Intent(context, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-
-        context.startActivity(intent)
-        // Wait for the app to appear
-        device.wait(
-            Until.hasObject(By.pkg(SAMPLE_PACKAGE).depth(0)),
-            LAUNCH_TIMEOUT
-        )
-
-        device.wait(
-            Until.hasObject(By.clazz(MainActivity::class.java).depth(0)),
-            LAUNCH_TIMEOUT
-        )
-
-        device.wait(
-            Until.hasObject(By.textContains("Time Remaining")),
-            LAUNCH_TIMEOUT
-        )
-
-        val selectedTimeTV: UiObject2? = device.findObject(By.textContains("Time Remaining"))
+        val selectedTimeTV: UiObject2? = device.wait(Until.findObject(By.textContains("Time Remaining")), LAUNCH_TIMEOUT)
         assertThat(selectedTimeTV, notNullValue())
 
         // Act
-        device.wait(
-            Until.hasObject(By.text("Select Time").depth(0)),
-            LAUNCH_TIMEOUT
-        )
-
-        val spinner: UiObject2? = device.findObject(By.text("Select Time"))
+        val spinner: UiObject2? = device.wait(Until.findObject(By.text("Select Time")), LAUNCH_TIMEOUT)
         assertThat(spinner, notNullValue())
 
         spinner?.click()
 
-        device.wait(
-            Until.hasObject(By.text("55")),
-            LAUNCH_TIMEOUT
-        )
-
-        val spinnerOption: UiObject2? = device.findObject(By.text("55"))
+        val spinnerOption: UiObject2? = device.wait(Until.findObject(By.text("55")), LAUNCH_TIMEOUT)
         assertThat(spinnerOption, notNullValue())
 
         spinnerOption?.click()
 
         assertThat(selectedTimeTV!!.text, `is`("55"))
-
     }
 
 }
